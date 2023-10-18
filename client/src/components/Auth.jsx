bbfa1df87c4a5bac7bd7d0b357dd7632184586e3
blaze308 <jaimepinkrah@gmail.com>
2023-10-18 01:35:04 +0000

    second commit

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -2,17 +2,38 @@ import React, { useState } from "react";
 
 import signupImage from "../assets/signup.jpg";
 
+const initialState = {
+	fullName: "",
+	username: "",
+	password: "",
+	confirmPassword: "",
+	phoneNumber: "",
+	avatarUrl: "",
+};
+
 const Auth = () => {
+	const [form, setForm] = useState(initialState);
 	const [isSignup, setIsSignup] = useState(true);
 
-	const handleChange = () => {};
+	const handleChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(form);
+	};
+
+	const switchMode = () => {
+		setIsSignup((prevIsSignup) => !prevIsSignup);
+	};
 
 	return (
 		<div className="auth__form-container">
 			<div className="auth__form-container_fields">
 				<div className="auth__form-container_fields-content">
 					<p>{isSignup ? "Sign Up" : "Sign In"}</p>
-					<form onSubmit={() => {}} action="">
+					<form onSubmit={handleSubmit} action="">
 						{isSignup && (
 							<div className="auth__form-container_fields-content_input">
 								<label htmlFor="fullName">Full Name</label>
@@ -81,9 +102,24 @@ const Auth = () => {
 								/>
 							</div>
 						)}
+						<div className="auth__form-container_fields-content_button">
+							<button onClick={handleSubmit}>
+								{isSignup ? "Sign Up" : "Sign In"}
+							</button>
+						</div>
 					</form>
-					<div className="auth__form-container_fields-account"></div>
+					<div className="auth__form-container_fields-account">
+						<p>
+							{isSignup ? "Already have an account?" : "Don't have an account?"}
+							<span onClick={switchMode}>
+								{isSignup ? "Sign In" : "Sign Up"}
+							</span>
+						</p>
+					</div>
 				</div>
+			</div>
+			<div className="auth__form-container_image">
+				<img src={signupImage} alt="sign in" />
 			</div>
 		</div>
 	);
